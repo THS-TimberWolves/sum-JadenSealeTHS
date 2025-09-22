ad8fc34b135937f313fef62784d274bdeed1cea7
JadenSealeTHS <sealejad000@alpinesd.org>
2025-09-22 11:00:45 -0600

    Update while_summation.py

--- while_summation.py
+++ while_summation.py
@@ -1,1 +1,11 @@
 #while summation code following directions from Readme
+x = int(input())
+stored_num = 0
+while x != 0:
+    if x < 0:
+        stored_num = stored_num + x
+        x += 1
+    elif x > 0:
+        stored_num = stored_num + x
+        x -= 1
+print(stored_num)
